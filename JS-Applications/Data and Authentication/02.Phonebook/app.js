70e7af348e8b12946c013e44764e0edd76a8acc3
AndreaNikolchova <a.nikolchova04@gmail.com>
2022-11-17 13:06:54 +0200

    New!

--- JS-Applications/Data and Authentication/02.Phonebook/app.js
+++ JS-Applications/Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    console.log('TODO...');
+    let buttonLoad = document.getElementById('btnLoad');
+    buttonLoad.addEventListener('click',async()=>{
+        let promiseLoad = await fetch('http://localhost:3030/jsonstore/phonebook');
+        let dataLoad = await promiseLoad.json();
+        let ul = document.getElementById('phonebook');
+        for(let phone in dataLoad){
+            let li = document.createElement('li');
+            li.textContent = dataLoad[phone].person +': '+  dataLoad[phone].phone;
+            let buttonDelete = document.createElement('button');
+            buttonDelete.textContent = 'Delete';
+            li.appendChild(buttonDelete);
+            buttonDelete.addEventListener('click',async()=>{
+                await fetch(`http://localhost:3030/jsonstore/phonebook/${dataLoad[phone]._id}`,{
+                    method:'delete',
+                });
+                ul.removeChild(li);
+            });
+            ul.appendChild(li);
+        }
+    });
+    let buttonCreate = document.getElementById('btnCreate');
+    buttonCreate.addEventListener('click',async()=>{
+        let data = {
+            person: document.getElementById('person').value,
+            phone: document.getElementById('phone').value
+          }
+          
+        await fetch('http://localhost:3030/jsonstore/phonebook',{
+            method: 'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(data),
+        });
+        
+
+    });
 }
 
 attachEvents();
--- JS-Applications/Data and Authentication/03.Students/app.js
+++ JS-Applications/Data and Authentication/03.Students/app.js
@@ -1,1 +1,52 @@
-console.log('TODO...');
+async function table(){
+    let tbody =  document.getElementsByTagName('tbody')[0];
+    let promise = await fetch(`http://localhost:3030/jsonstore/collections/students`);
+    let data = await promise.json();
+    for(let obj in data){
+        let tr = document.createElement('tr');
+        let tdFirstName = document.createElement('td');
+        tdFirstName.textContent = data[obj].firstName;
+        let tdLastName = document.createElement('td');
+        tdLastName.textContent = data[obj].lastName;
+        let tdFacultyNumber = document.createElement('td');
+        tdFacultyNumber.textContent = data[obj].facultyNumber;
+        let tdGrade = document.createElement('td');
+        tdGrade.textContent = data[obj].grade;
+        tr.appendChild(tdFirstName);
+        tr.appendChild(tdLastName);
+        tr.appendChild(tdFacultyNumber);
+        tr.appendChild(tdGrade);
+        tbody.appendChild(tr);
+    }
+    let button = document.getElementById('submit');
+    button.addEventListener('click',async()=>{
+        let student = {
+            firstName: document.getElementsByTagName('input')[0].value,
+            lastName: document.getElementsByTagName('input')[1].value,
+            facultyNumber: document.getElementsByTagName('input')[2].value,
+            grade: document.getElementsByTagName('input')[3].value,
+
+        }
+        await fetch(`http://localhost:3030/jsonstore/collections/students`,{
+            method:'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(student)
+        });
+        let tr = document.createElement('tr');
+        let tdFirstName = document.createElement('td');
+        tdFirstName.textContent = student.firstName;
+        let tdLastName = document.createElement('td');
+        tdLastName.textContent = student.lastName;
+        let tdFacultyNumber = document.createElement('td');
+        tdFacultyNumber.textContent = student.facultyNumber;
+        let tdGrade = document.createElement('td');
+        tdGrade.textContent = student.grade;
+        tr.appendChild(tdFirstName);
+        tr.appendChild(tdLastName);
+        tr.appendChild(tdFacultyNumber);
+        tr.appendChild(tdGrade);
+        tbody.appendChild(tr);
+    });
+
+}
+table();
